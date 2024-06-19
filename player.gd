c1b30cc096b6bdc519a574ecd431283e7c7cdfbe
chiayunrong <dev@chiayunrong.com>
2024-06-20 07:51:13 +0800

    Synchronize player_id

--- player.gd
+++ player.gd
@@ -2,13 +2,11 @@ extends CharacterBody2D
 
 const SPEED = 400
 
-var player_id := 1:
+@export var player_id := 1:
 	set(id):
 		player_id = id
+		$InputSynchronizer.set_multiplayer_authority(player_id)
 
-func _enter_tree():
-	$InputSynchronizer.set_multiplayer_authority(player_id)
-		
 func _ready():
 	$Camera2D.enabled = false
 
